f85033819295e4f98cedac1ebe1d27d06aa55ceb
na.perruchot <na.perruchot@gmail.com>
2016-02-23 21:21:54 +0100

    refacto

--- main/java/com/esgi/googlenews/Modeles/ArticleAdapter.java
+++ main/java/com/esgi/googlenews/Modeles/ArticleAdapter.java
@@ -1,6 +1,7 @@
 package com.esgi.googlenews.Modeles;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
